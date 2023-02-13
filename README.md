19a5aadf04e7cb123039120dcac8967de6eb2c43
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:02 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Dynamic Chart Dashboard
 
-![JavaScript Dynamic Chart Dashboard](dashboardDynamic.png)
+![JavaScript Dynamic Chart Dashboard](dashboardDynamic-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,17 +8,16 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
-
 
 Example on how a dynamic chart dashboard can be created with LightningChart JS.
 
@@ -29,6 +28,7 @@ This can be achieved out of the box with LightningChart by placing each chart in
 The performance problem with placing each chart in its own DIV is that all plotting preparation has to be done separately. LC JS Dashboard was specifically designed to be the answer to this problem - it allows efficient sharing of plotting resources and processes to greatly increase the loading speed of charts (with 10 charts, up to 100% faster) as well as run-time performance.
 
 Generally, LC JS Dashboards are static sized (for example, 2x2 grid) and size can't be changed afterwards. However, with some creative coding this limitation can be worked around by preallocating the Dashboard for some number of "maximum charts" and then programmatically resizing the Dashboard cells which actually have charts in them, which is exactly what we do in this example.
+
 
 ## API Links
 
@@ -43,26 +43,26 @@ Generally, LC JS Dashboards are static sized (for example, 2x2 grid) and size ca
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[Dashboard]: https://www.arction.com/lightningchart-js-api-documentation/v3.3.0/classes/dashboard.html
-[XY cartesian chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.3.0/classes/chartxy.html
-[Line series]: https://www.arction.com/lightningchart-js-api-documentation/v3.3.0/classes/lineseries.html
-[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.3.0/classes/axis.html
-[Translate point]: https://www.arction.com/lightningchart-js-api-documentation/v3.3.0/globals.html#translatepoint
+[Dashboard]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Dashboard.html
+[XY cartesian chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[Line series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LineSeries.html
+[Axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis.html
+[Translate point]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/translatePoint.html
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 /**
  * Example on how a dynamic chart dashboard can be created with LightningChart JS.
- * 
+ *
  * The use case is an application where charts can be added and removed at users whim to perform different kinds of analysis within a dynamic application setting.
- * 
+ *
  * This can be achieved out of the box with LightningChart by placing each chart in its own HTML DIV,
  * but this performs slightly worse than if the charts would be inside a Dashboard component.
- * 
+ *
  * By using a Dashboard, the rendering engine for all Charts can be reused leading to much more faster redrawing and most importantly initial loading speed.
  *
  * Generally, LC JS Dashboards are static sized (for example, 2x2 grid) and size can't be changed afterwards.
@@ -13,144 +13,135 @@
  * and then programmatically resizing the Dashboard cells which actually have charts in them.
  */
 
-const lcjs = require("@arction/lcjs");
-const xydata = require("@arction/xydata");
-const { lightningChart, emptyLine, translatePoint, Themes } = lcjs;
-const { createProgressiveTraceGenerator } = xydata;
+const lcjs = require('@arction/lcjs')
+const xydata = require('@arction/xydata')
+const { lightningChart, emptyLine, translatePoint, Themes } = lcjs
+const { createProgressiveTraceGenerator } = xydata
 
 const exampleContainer = document.getElementById('chart') || document.body
 
-const mainDiv = document.createElement("div");
-exampleContainer.append(mainDiv);
-mainDiv.style.position = "absolute";
-mainDiv.style.width = "100%";
-mainDiv.style.height = "100%";
-mainDiv.style.display = "flex";
-mainDiv.style.flexDirection = "row";
-mainDiv.style.color = "black";
+const mainDiv = document.createElement('div')
+exampleContainer.append(mainDiv)
+mainDiv.style.position = 'absolute'
+mainDiv.style.width = '14.5%'
+mainDiv.style.height = 'auto'
+mainDiv.style.display = 'flex'
+mainDiv.style.justifyContent = 'center'
+mainDiv.style.flexDirection = 'row'
+mainDiv.style.color = 'black'
+mainDiv.style.zIndex = '1'
 
-const uiDiv = document.createElement("div");
-mainDiv.append(uiDiv);
-uiDiv.style.display = "flex";
-uiDiv.style.flexDirection = "column";
-uiDiv.style.backgroundColor = exampleContainer.parentElement.parentElement && window.getComputedStyle(exampleContainer.parentElement.parentElement).backgroundColor
-uiDiv.style.color = exampleContainer.parentElement.parentElement && window.getComputedStyle(exampleContainer.parentElement.parentElement).color
+const uiDiv = document.createElement('div')
+mainDiv.append(uiDiv)
+uiDiv.style.display = 'flex'
+uiDiv.style.flexDirection = 'column'
+uiDiv.style.backgroundColor = 'transparent'
+uiDiv.style.color = exampleContainer.parentElement && window.getComputedStyle(exampleContainer.parentElement).color
 
-const uiDivTitle = document.createElement("span");
-uiDiv.append(uiDivTitle);
-uiDivTitle.innerHTML = "Click to add graph";
-
-const chartDiv = document.createElement("div");
-mainDiv.append(chartDiv);
-chartDiv.style.flexGrow = 1;
-(async () => {
-  for (let i = 0; i < 6; i += 1) {
-    const label = `Measurement ${new Date(
-      Date.now() - i * 24 * 60 * 60 * 1000
-    ).toLocaleDateString()}`;
-    const data = await createProgressiveTraceGenerator()
-      .setNumberOfPoints(200)
-      .generate()
-      .toPromise();
-
-    const buttonAddChart = document.createElement("button");
-    uiDiv.append(buttonAddChart);
-    buttonAddChart.innerHTML = label;
-    buttonAddChart.style.whiteSpace = "nowrap";
-    buttonAddChart.addEventListener("click", (e) => {
-      addGraph(label, data);
-    });
-  }
-})();
+const uiDivTitle = document.createElement('span')
+uiDiv.append(uiDivTitle)
+uiDivTitle.innerHTML = 'Click to add graph'
 
 // NOTE: When this is exceeded dashboard would have to be recreated again with all charts and data.
 // There is no existing identified limit for this value though.
-const maxCellsCount = 5;
+const maxCellsCount = 5
 
 const dashboard = lightningChart()
-  .Dashboard({
-    container: chartDiv,
-    numberOfColumns: 1,
-    numberOfRows: maxCellsCount,
-    disableAnimations: true,
-    // theme: Themes.darkGold
-  })
-  .setSplitterStyle(emptyLine)
-  .setSplitterStyleHighlight(emptyLine);
+    .Dashboard({
+        numberOfColumns: 2,
+        numberOfRows: maxCellsCount,
+        // theme: Themes.darkGold
+    })
+    .setSplitterStyle(emptyLine)
+    .setColumnWidth(0, 1)
+    .setColumnWidth(1, 6)
 
-const charts = [];
+const charts = []
 
 const updateDashboardRowHeights = () => {
-  for (let row = 0; row < maxCellsCount; row += 1) {
-    dashboard.setRowHeight(row, 0.00001);
-  }
-  charts.forEach((chart) => dashboard.setRowHeight(chart.row, 1));
-};
+    for (let row = 0; row < maxCellsCount; row += 1) {
+        dashboard.setRowHeight(row, 0.00001)
+    }
+    charts.forEach((chart) => dashboard.setRowHeight(chart.row, 1))
+}
 
 const addGraph = (name, data) => {
-  const freeRow = new Array(maxCellsCount)
-    .fill(0)
-    .findIndex(
-      (_, row) => charts.find((item) => item.row === row) === undefined
-    );
-  if (freeRow < 0) {
-    // Dashboard has no more row slots.
-    alert(`Dashboard is not allocated for more than ${maxCellsCount} graphs`);
-    return;
-  }
+    const freeRow = new Array(maxCellsCount).fill(0).findIndex((_, row) => charts.find((item) => item.row === row) === undefined)
+    if (freeRow < 0) {
+        // Dashboard has no more row slots.
+        alert(`Dashboard is not allocated for more than ${maxCellsCount} graphs`)
+        return
+    }
 
-  const chart = dashboard
-    .createChartXY({
-      columnIndex: 0,
-      rowIndex: freeRow,
+    const chart = dashboard
+        .createChartXY({
+            columnIndex: 1,
+            columnSpan: 1,
+            rowIndex: freeRow,
+        })
+        .setTitle(name)
+        .setTitleFont((font) => font.setSize(12))
+        .setTitleMargin({ top: 0, bottom: 0 })
+        .setPadding({ top: 0 })
+
+    const buttonRemoveChart = document.createElement('button')
+    document.body.append(buttonRemoveChart)
+    buttonRemoveChart.innerHTML = 'X'
+    buttonRemoveChart.style.position = 'absolute'
+    buttonRemoveChart.style.zIndex = '1'
+
+    buttonRemoveChart.addEventListener('click', (e) => {
+        chart.dispose()
+        document.body.removeChild(buttonRemoveChart)
+        charts.splice(
+            charts.findIndex((item) => item.chart === chart),
+            1,
+        )
+        updateDashboardRowHeights()
+        charts.forEach((item) => item.updateRemoveButtonPosition())
     })
-    .setTitle(name)
-    .setTitleFont((font) => font.setSize(12))
-    .setTitleMarginBottom(0)
-    .setTitleMarginTop(0)
-    .setPadding({ top: 0 });
 
-  const buttonRemoveChart = document.createElement("button");
-  document.body.append(buttonRemoveChart);
-  buttonRemoveChart.innerHTML = "X";
-  buttonRemoveChart.style.position = "absolute";
-  buttonRemoveChart.addEventListener("click", (e) => {
-    chart.dispose();
-    document.body.removeChild(buttonRemoveChart);
-    charts.splice(
-      charts.findIndex((item) => item.chart === chart),
-      1
-    );
-    updateDashboardRowHeights();
-    charts.forEach((item) => item.updatePosition());
-  });
+    const updateRemoveButtonPosition = () => {
+        const posChartEngine = translatePoint({ x: 100, y: 100 }, chart.uiScale, chart.engine.scale)
+        const posChart = chart.engine.engineLocation2Client(posChartEngine.x, posChartEngine.y)
+        buttonRemoveChart.style.left = `${posChart.x - buttonRemoveChart.offsetWidth}px`
+        buttonRemoveChart.style.top = `${posChart.y}px`
+    }
 
-  const updatePosition = () => {
-    const posEngine = translatePoint(
-      { x: 0, y: 100 },
-      chart.uiScale,
-      chart.engine.scale
-    );
-    const posDocument = chart.engine.engineLocation2Client(
-      posEngine.x,
-      posEngine.y
-    );
-    buttonRemoveChart.style.left = `${posDocument.x}px`;
-    buttonRemoveChart.style.top = `${posDocument.y}px`;
-  };
+    const series = chart
+        .addPointLineSeries({
+            dataPattern: { pattern: 'ProgressiveX' },
+        })
+        .setName(name)
+        .add(data)
 
-  const series = chart
-    .addPointLineSeries({
-      dataPattern: { pattern: "ProgressiveX" },
+    charts.push({
+        row: freeRow,
+        chart,
+        updateRemoveButtonPosition,
     })
-    .setName(name)
-    .add(data);
+    updateDashboardRowHeights()
+    charts.forEach((item) => item.updateRemoveButtonPosition())
+}
 
-  charts.push({
-    row: freeRow,
-    chart,
-    updatePosition,
-  });
-  updateDashboardRowHeights();
-  charts.forEach((item) => item.updatePosition());
-};
+;(async () => {
+    for (let i = 0; i < 6; i += 1) {
+        const label = `Measurement ${new Date(Date.now() - i * 24 * 60 * 60 * 1000).toLocaleDateString()}`
+        const data = await createProgressiveTraceGenerator().setNumberOfPoints(200).generate().toPromise()
+
+        const buttonAddChart = document.createElement('button')
+        uiDiv.append(buttonAddChart)
+        buttonAddChart.innerHTML = label
+        buttonAddChart.style.margin = '4px 0'
+        buttonAddChart.addEventListener('click', (e) => {
+            addGraph(label, data)
+        })
+        if (i < 3) addGraph(label, data)
+    }
+})()
+
+window.addEventListener('resize', () => {
+    requestAnimationFrame(() => {
+        charts.forEach((item) => item.updateRemoveButtonPosition())
+    })
+})
